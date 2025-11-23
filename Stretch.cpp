4a34943159d9ee0df4a46c9e4d6aac7e40ec214f
rebuildup <rebuild.up.up@gmail.com>
2025-11-23 14:46:13 +0900

    refactor: Smart Render関連の定義と機能を削除し、クラシックレンダーに切り替えました。

--- Stretch.cpp
+++ Stretch.cpp
@@ -28,9 +28,8 @@ static PF_Err
 GlobalSetup(PF_InData *in_data, PF_OutData *out_data, PF_ParamDef *params[], PF_LayerDef *output)
 {
     out_data->my_version = PF_VERSION(MAJOR_VERSION, MINOR_VERSION, BUG_VERSION, STAGE_VERSION, BUILD_VERSION);
-    // Support 16-bit (Deep Color) and 32-bit Float
-    out_data->out_flags = PF_OutFlag_DEEP_COLOR_AWARE | PF_OutFlag_PIX_INDEPENDENT;
-    out_data->out_flags2 = PF_OutFlag2_FLOAT_COLOR_AWARE | PF_OutFlag2_SUPPORTS_SMART_RENDER | PF_OutFlag2_SUPPORTS_THREADED_RENDERING;
+    // Support 16-bit (Deep Color) - Classic Render
+    out_data->out_flags = PF_OutFlag_DEEP_COLOR_AWARE;
     return PF_Err_NONE;
 }
 
@@ -77,109 +76,7 @@ static PF_Err ParamsSetup(PF_InData *in_data, PF_OutData *out_data, PF_ParamDef 
     return PF_Err_NONE;
 }
 
-static void UnionLRect(const PF_LRect* src, PF_LRect* dst) {
-    if (src->left < dst->left) dst->left = src->left;
-    if (src->top < dst->top) dst->top = src->top;
-    if (src->right > dst->right) dst->right = src->right;
-    if (src->bottom > dst->bottom) dst->bottom = src->bottom;
-}
 
-static PF_Err
-PreRender(
-    PF_InData* in_data,
-    PF_OutData* out_data,
-    void* extraP)
-{
-    PF_Err err = PF_Err_NONE;
-    PF_PreRenderExtra_Local* extra = (PF_PreRenderExtra_Local*)extraP;
-    PF_RenderRequest_Local req = extra->input->output_request;
-    PF_CheckoutResult_Local in_result;
-
-    // Checkout input
-    ERR(extra->cb->checkout_layer(in_data->effect_ref,
-        STRETCH_INPUT,
-        STRETCH_INPUT,
-        &req,
-        in_data->current_time,
-        in_data->time_step,
-        in_data->time_scale,
-        &in_result));
-
-    // Set result rects
-    if (!err) {
-        UnionLRect(&in_result.result_rect, &extra->output->result_rect);
-        UnionLRect(&in_result.max_result_rect, &extra->output->max_result_rect);
-    }
-
-    return err;
-}
-
-static PF_Err
-SmartRender(
-    PF_InData* in_data,
-    PF_OutData* out_data,
-    void* extraP)
-{
-    PF_Err err = PF_Err_NONE;
-    PF_SmartRenderExtra_Local* extra = (PF_SmartRenderExtra_Local*)extraP;
-    PF_EffectWorld* input_world = NULL;
-    PF_EffectWorld* output_world = NULL;
-    
-    // Use SuiteHandler for cleaner suite acquisition
-    AEGP_SuiteHandler suites(in_data->pica_basicP);
-    PF_WorldSuite2* wsP = NULL;
-    ERR(suites.SPBasicSuite()->AcquireSuite(kPFWorldSuite, kPFWorldSuiteVersion2, (const void**)&wsP));
-
-    if (!err) {
-        // Checkout input/output
-        ERR((extra->cb->checkout_layer_pixels(in_data->effect_ref, STRETCH_INPUT, &input_world)));
-        ERR(extra->cb->checkout_output(in_data->effect_ref, &output_world));
-    }
-
-    if (!err && input_world && output_world) {
-        // Checkout parameters
-        PF_ParamDef p[STRETCH_NUM_PARAMS];
-        PF_ParamDef* pp[STRETCH_NUM_PARAMS];
-        
-        // Input
-        AEFX_CLR_STRUCT(p[STRETCH_INPUT]);
-        p[STRETCH_INPUT].u.ld = *input_world;
-        pp[STRETCH_INPUT] = &p[STRETCH_INPUT];
-
-        // Params
-        for (int i = 1; i < STRETCH_NUM_PARAMS; ++i) {
-            PF_Checkout_Value(in_data, out_data, i, in_data->current_time, in_data->time_step, in_data->time_scale, &p[i]);
-            pp[i] = &p[i];
-        }
-
-        // Call Render
-        // Check bit depth
-        int bpp = (output_world->width > 0) ? (output_world->rowbytes / output_world->width) : 0;
-        if (bpp == sizeof(PF_PixelFloat)) {
-            err = RenderGeneric<PF_PixelFloat>(in_data, out_data, pp, output_world);
-        } else if (bpp == sizeof(PF_Pixel16)) {
-            err = RenderGeneric<PF_Pixel16>(in_data, out_data, pp, output_world);
-        } else {
-            err = RenderGeneric<PF_Pixel>(in_data, out_data, pp, output_world);
-        }
-        
-        // Checkin params
-        for (int i = 1; i < STRETCH_NUM_PARAMS; ++i) {
-            PF_Checkin_Param(in_data, out_data, i, &p[i]);
-        }
-    }
-    
-    // Release suite if acquired manually, but SuiteHandler doesn't own this one unless we use a helper method.
-    // However, SuiteHandler is usually for AEGP suites.
-    // For PF suites, we often use manual acquire or a specific helper.
-    // But wait, SuiteHandler destructor releases suites it acquired via its methods.
-    // If we use SPBasicSuite()->AcquireSuite, we must release manually.
-    if (wsP) {
-        suites.SPBasicSuite()->ReleaseSuite(kPFWorldSuite, kPFWorldSuiteVersion2);
-    }
-
-    return err;
-}
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
@@ -532,8 +429,7 @@ PF_Err EffectMain(PF_Cmd cmd,
             case PF_Cmd_ABOUT: err = About(in_data, out_data, params, output); break;
             case PF_Cmd_GLOBAL_SETUP: err = GlobalSetup(in_data, out_data, params, output); break;
             case PF_Cmd_PARAMS_SETUP: err = ParamsSetup(in_data, out_data, params, output); break;
-            case PF_Cmd_SMART_PRE_RENDER: err = PreRender(in_data, out_data, extra); break;
-        case PF_Cmd_SMART_RENDER: err = SmartRender(in_data, out_data, extra); break;
+            case PF_Cmd_RENDER: err = Render(in_data, out_data, params, output); break;
             default: break;
         }
     } catch (...) {
--- Stretch.h
+++ Stretch.h
@@ -26,62 +26,6 @@
 #endif
 #endif
 
-// Define Smart Render structs locally to avoid missing header issues
-typedef A_long PF_Field;
-typedef A_Matrix4 PF_ChannelMatrix;
-
-typedef struct PF_SmartRenderCallbacks_Local {
-    PF_Err (*checkout_layer_pixels)(PF_ProgPtr effect_ref, PF_ParamIndex index, PF_EffectWorld **pixels);
-    PF_Err (*checkout_output)(PF_ProgPtr effect_ref, PF_EffectWorld **output);
-    PF_Err (*checkin_layer_pixels)(PF_ProgPtr effect_ref, PF_ParamIndex index);
-    PF_Err (*is_layer_pixel_data_valid)(PF_ProgPtr effect_ref, PF_ParamIndex index, PF_Boolean *valid);
-} PF_SmartRenderCallbacks_Local;
-
-typedef struct PF_SmartRenderExtra_Local {
-    PF_SmartRenderCallbacks_Local *cb;
-    void *unused;
-} PF_SmartRenderExtra_Local;
-
-typedef struct PF_RenderRequest_Local {
-    PF_LRect rect;
-    PF_Field field;
-    PF_ChannelMatrix channel_matrix;
-    PF_Boolean preserve_rgb_of_zero_alpha;
-    PF_Boolean preserve_rgb_of_zero_alpha_is_valid;
-    void *unused;
-} PF_RenderRequest_Local;
-
-typedef struct PF_CheckoutResult_Local {
-    PF_LRect result_rect;
-    PF_LRect max_result_rect;
-    PF_Boolean par_varying;
-    PF_Boolean par_varying_is_valid;
-    void *unused;
-} PF_CheckoutResult_Local;
-
-typedef struct PF_PreRenderInput_Local {
-    PF_RenderRequest_Local output_request;
-    short bit_depth;
-    void *unused;
-} PF_PreRenderInput_Local;
-
-typedef struct PF_PreRenderOutput_Local {
-    PF_LRect result_rect;
-    PF_LRect max_result_rect;
-    void *unused;
-} PF_PreRenderOutput_Local;
-
-typedef struct PF_PreRenderCallbacks_Local {
-    PF_Err (*checkout_layer)(PF_ProgPtr effect_ref, PF_ParamIndex index, PF_ParamIndex req_index, const PF_RenderRequest_Local *req, A_long current_time, A_long time_step, A_u_long time_scale, PF_CheckoutResult_Local *result);
-    PF_Err (*checkout_layer_pixels)(PF_ProgPtr effect_ref, PF_ParamIndex index, PF_ParamIndex req_index, const PF_RenderRequest_Local *req, A_long current_time, A_long time_step, A_u_long time_scale, PF_EffectWorld **pixels);
-} PF_PreRenderCallbacks_Local;
-
-typedef struct PF_PreRenderExtra_Local {
-    PF_PreRenderCallbacks_Local *cb;
-    PF_PreRenderInput_Local *input;
-    PF_PreRenderOutput_Local *output;
-    void *unused;
-} PF_PreRenderExtra_Local;
 
 #ifndef DllExport
 #if defined(_WIN32)
