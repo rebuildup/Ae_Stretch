e41744af3ac58bf89c8ae36589655db75324fe39
rebuildup <rebuild.up.up@gmail.com>
2025-11-24 00:56:36 +0900

    Fix Stretch PiPL: change name and category
    - Change plugin Name from 'Stretch_v2' to 'Stretch'
    - Change Category from 'Stretch' to '361do_plugins'
    - Fixes plugin loading issues in After Effects

---
+++ SkeletonPiPL.r
@@ -0,0 +1,70 @@
+#include "AEConfig.h"
+#include "AE_EffectVers.h"
+
+#ifndef AE_OS_WIN
+	#include <AE_General.r>
+#endif
+	
+resource 'PiPL' (16000) {
+	{	
+		Kind {
+			AEEffect
+		},
+		
+		Name {
+			"Stretch"
+		},
+		
+		Category {
+			"361do_plugins"
+		},
+#ifdef AE_OS_WIN
+	#ifdef AE_PROC_INTELx64
+		CodeWin64X86 {"EffectMain"},
+	#endif
+#else
+	#ifdef AE_OS_MAC
+		CodeMacIntel64 {"EffectMain"},
+		CodeMacARM64 {"EffectMain"},
+	#endif
+#endif
+		
+		AE_PiPL_Version {
+			2,
+			0
+		},
+		
+		AE_Effect_Spec_Version {
+			PF_PLUG_IN_VERSION,
+			PF_PLUG_IN_SUBVERS
+		},
+		
+		AE_Effect_Version {
+			557057	/* 1.1 */
+		},
+		
+		AE_Effect_Info_Flags {
+			0
+		},
+		
+		AE_Effect_Global_OutFlags {
+			0x02000400
+		},
+		
+		AE_Effect_Global_OutFlags_2 {
+			0x08000000
+		},
+		
+		AE_Effect_Match_Name {
+			"361do Stretch"
+		},
+		
+		AE_Reserved_Info {
+			0
+		},
+		
+		AE_Effect_Support_URL {
+			"https://github.com/rebuildup/Ae_Stretch"
+		}
+	}
+};
