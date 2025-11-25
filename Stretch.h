8912f2263b345a3c99df55b267720fb8be299d56
rebuildup <rebuild.up.up@gmail.com>
2025-11-25 20:57:07 +0900

    fix fix fix version

--- Stretch.h
+++ Stretch.h
@@ -2,15 +2,57 @@
 #ifndef STRETCH_H
 #define STRETCH_H
 
+#include "AEConfig.h"
+
+#define MAJOR_VERSION 1
+#define MINOR_VERSION 2
+#define BUG_VERSION 0
+#define BUILD_VERSION 1
+#define NAME "Stretch"
+#define DESCRIPTION "Stretches pixels based on an anchor point and angle"
+
+#ifdef AE_STRETCH_PIPL_BUILD
+
+#ifndef PF_Stage_DEVELOP
+#define PF_Stage_DEVELOP 0
+#endif
+#define STAGE_VERSION 0
+
+#ifndef PF_Vers_VERS_BITS
+#define PF_Vers_VERS_BITS 0x7L
+#define PF_Vers_VERS_SHIFT 19
+#define PF_Vers_VERS_HIGH_BITS 0xfL
+#define PF_Vers_VERS_HIGH_SHIFT 26
+#define PF_Vers_VERS_LOW_SHIFT 3
+#define PF_Vers_SUBVERS_BITS 0xfL
+#define PF_Vers_SUBVERS_SHIFT 15
+#define PF_Vers_BUGFIX_BITS 0xfL
+#define PF_Vers_BUGFIX_SHIFT 11
+#define PF_Vers_STAGE_BITS 0x3L
+#define PF_Vers_STAGE_SHIFT 9
+#define PF_Vers_BUILD_BITS 0x1ffL
+#define PF_Vers_BUILD_SHIFT 0
+#endif
+
+#define STRETCH_VERSION                                                                                    \
+    (((((MAJOR_VERSION >> PF_Vers_VERS_LOW_SHIFT) & PF_Vers_VERS_HIGH_BITS) << PF_Vers_VERS_HIGH_SHIFT)) | \
+     ((((MAJOR_VERSION) & PF_Vers_VERS_BITS) << PF_Vers_VERS_SHIFT)) |                                     \
+     ((((MINOR_VERSION) & PF_Vers_SUBVERS_BITS) << PF_Vers_SUBVERS_SHIFT)) |                               \
+     ((((BUG_VERSION) & PF_Vers_BUGFIX_BITS) << PF_Vers_BUGFIX_SHIFT)) |                                   \
+     ((((STAGE_VERSION) & PF_Vers_STAGE_BITS) << PF_Vers_STAGE_SHIFT)) |                                   \
+     ((((BUILD_VERSION) & PF_Vers_BUILD_BITS) << PF_Vers_BUILD_SHIFT)))
+
+#else
+
 #ifdef _WIN32
 #define _USE_MATH_DEFINES
 #define NOMINMAX
 #endif
 
-#include "AEConfig.h"
 #include "AE_Effect.h"
+#define STAGE_VERSION PF_Stage_DEVELOP
+#define STRETCH_VERSION PF_VERSION(MAJOR_VERSION, MINOR_VERSION, BUG_VERSION, STAGE_VERSION, BUILD_VERSION)
 
-#if !defined(AE_STRETCH_PIPL_BUILD)
 #include "entry.h"
 #include "AE_EffectCB.h"
 #include "AE_Macros.h"
@@ -20,9 +62,8 @@
 #include "AE_GeneralPlug.h"
 #include "AEGP_SuiteHandler.h"
 #include <cmath>
-#endif
 
-#if defined(_WIN32) && !defined(AE_STRETCH_PIPL_BUILD)
+#if defined(_WIN32)
 #include <Windows.h>
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
@@ -36,15 +77,6 @@
 #define DllExport
 #endif
 #endif
-
-#define MAJOR_VERSION 1
-#define MINOR_VERSION 2
-#define BUG_VERSION 0
-#define STAGE_VERSION PF_Stage_DEVELOP
-#define BUILD_VERSION 1
-#define STRETCH_VERSION PF_VERSION(MAJOR_VERSION, MINOR_VERSION, BUG_VERSION, STAGE_VERSION, BUILD_VERSION)
-#define NAME "Stretch"
-#define DESCRIPTION "Stretches pixels based on an anchor point and angle"
 
 // String IDs
 enum
@@ -98,5 +130,7 @@ extern "C"
 #ifdef __cplusplus
 }
 #endif
+
+#endif // AE_STRETCH_PIPL_BUILD
 
 #endif // STRETCH_H
